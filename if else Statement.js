aa8328d731d0f792f97acac41561cb9742f96cfd
Talha Farhan <128322158+talha-farhan@users.noreply.github.com>
2023-07-19 00:29:43 +0500

    Create if else Statement.js

---
+++ if else Statement.js
@@ -0,0 +1,39 @@
+let GPA = 3.4;
+if(GPA >= 3){
+document.write("Eligible to sit in the test.")
+}else{
+document.write("Not eligible to sit in the exam. Receive resume only.")
+}
+document.write("<br>")
+
+let numberOfBuses = 2;
+if(numberOfBuses >= 5){
+document.write("Good to go.")
+}else{
+document.write("Return back!")
+}
+document.write("<br>")
+
+let equalSides = 1
+if(equalSides <= 2){
+document.write("Isosceles Triangle")
+}else{
+document.write("Not iscosceles triangle")
+}
+document.write("<br>")
+
+let x = "100"
+if(x === 100){
+document.write("x is same")
+}else{
+document.write("x is different")
+} //Here the data type is also being checked
+document.write("<br>")
+
+let name = "Talha Farhan"
+let gender = "Male"
+if(gender = "Male"){
+document.write("Hello Mr." + name)
+}else{
+document.write("Hello Miss." + name)
+}
